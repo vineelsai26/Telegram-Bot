6163b9c671644414e0157e70eee93904064938c1
Vineel Sai <vineelsai5439@gmail.com>
2020-08-14 16:42:53 +0530

    test with port 80

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const http = require('http');
 const fs = require('fs');
 var exec = require('child_process').exec, child;
 
+const PORT = process.env.PORT || 80;
+
 fs.readFile('./index.html', function (err, html) {
     if (err) throw err;
     http.createServer(function (request, response) {
         response.writeHeader(200, { "Content-Type": "text/html" });
         response.write(html);
         response.end();
-    }).listen(8080);
+    }).listen(PORT);
     console.log('Bot is live');
 });
 
