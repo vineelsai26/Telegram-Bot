588e8807624658dce6a2345479eb7912e726f870
Vineel Sai <vineelsai5439@gmail.com>
2020-08-17 20:18:28 +0530

    fix text reply not working

--- features/text.js
+++ features/text.js
@@ -1,7 +1,8 @@
+const Telegraf = require('telegraf')
+
 module.exports.text = function text(ctx) {
     const msg = ctx.message.text.toLowerCase()
     if (msg == 'hi' || msg == 'hii' || msg == 'hello' || msg == 'hey') {
-        console.log(ctx.message.text.toLowerCase())
         if (ctx.from.username == 'vineelsai') {
             ctx.reply('Hi Boss', Telegraf.Extra.inReplyTo(ctx.message.message_id))
         } else {
