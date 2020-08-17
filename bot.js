4113700cc4e6e5c7847f13fa6b3666dff9ad41f5
Vineel Sai <vineelsai5439@gmail.com>
2020-08-17 18:48:24 +0530

    check for admins before doing some stuff

--- bot.js
+++ bot.js
@@ -12,7 +12,18 @@ bot.start((ctx) => ctx.reply('Welcome! I am F.R.I.D.A.Y Bot not sure what you ar
 
 bot.help((ctx) => ctx.reply('- send Hi or Hello \n- send sticker \n- /poll question option1 option2 .. \n- /github username \n- /unpin'))
 
-bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.use(function (ctx, next) {
+    if (ctx.chat.id > 0) return next()
+
+    return bot.telegram.getChatAdministrators(ctx.chat.id)
+        .then(function (data) {
+            if (!data || !data.length) return
+            ctx.chat._admins = data
+            ctx.from.isAdmin = data.some(adm => adm.user.id === ctx.from.id)
+        })
+        .catch(console.log)
+        .then(_ => next(ctx))
+})
 
 bot.command('echo', (ctx) => {
     const msg = ctx.message.text.replace('/echo ', '')
@@ -20,7 +31,11 @@ bot.command('echo', (ctx) => {
 })
 
 bot.command('poll', (ctx) => {
-    poll.poll(ctx)
+    if (ctx.from.isAdmin) {
+        poll.poll(ctx)
+    } else {
+        ctx.reply('You are not an admin')
+    }
 })
 
 bot.command('github', (ctx) => {
@@ -28,20 +43,36 @@ bot.command('github', (ctx) => {
 })
 
 bot.command('unpin', (ctx) => {
-    bot.telegram.unpinChatMessage(ctx.chat.id)
+    if (ctx.from.isAdmin){
+        bot.telegram.unpinChatMessage(ctx.chat.id)
+    } else {
+        ctx.reply('You are not an admin')
+    }
 })
 
-bot.on('text', (ctx) => {
-    text.text(ctx)
+bot.hears('I\'m admin', function (ctx) {
+    if (ctx.from.isAdmin) {
+        return ctx.reply('Yep!')
+    } else {
+        return ctx.reply('No, you are not')
+    }
+})
+
+bot.command('kickme', (ctx) => {
+    if (ctx.from.isAdmin) {
+        ctx.reply('You are an admin you can`t be kicked out rot here')
+    } else {
+        ctx.telegram.leaveChat(ctx.message.chat.id)
+        ctx.leaveChat()
+    }
 })
 
 bot.catch((err, ctx) => {
     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
 })
 
-bot.command('quit', (ctx) => {
-    ctx.telegram.leaveChat(ctx.message.chat.id)
-    ctx.leaveChat()
+bot.on('text', (ctx) => {
+    text.text(ctx)
 })
 
 bot.launch()
--- features/github.js
+++ features/github.js
@@ -4,6 +4,7 @@ module.exports.github =  function github(ctx) {
     const userName = ctx.message.text.replace('/github', '').trim()
     const client = octonode.client()
     const url = '/users/' + userName
+
     if (userName == '') {
         ctx.reply('command should be in formate /github username')
     } else if (userName.split(" ").length > 1) {
@@ -13,7 +14,14 @@ module.exports.github =  function github(ctx) {
             if (err !== null) {
                 ctx.reply(err)
             }
-            ctx.replyWithPhoto(body.avatar_url, { caption: 'Id : ' + body.id + '\n' + 'Name : ' + body.name + '\n' + 'User Name : ' + body.login + '\n' + 'Profile : ' + 'https://github.com/' + body.login })
+            ctx.replyWithPhoto(body.avatar_url, {
+                caption: 'Id : ' + body.id + '\n' + 
+                'Name : ' + body.name + '\n' + 
+                'User Name : ' + body.login + '\n' +
+                'Profile : ' + 'https://github.com/' + body.login + '\n' + 
+                'Followers : ' + body.followers + '\n' +
+                'Repositories : ' + body.public_repos
+            })
         })
     }
 }
